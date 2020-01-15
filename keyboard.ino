f425c0402f2baea03dd8fefc6d41f70eed154bf1
cvasqxz <cvxz@pm.me>
2020-01-15 17:46:46 -0300

    hola mundo

--- keyboard.ino
+++ keyboard.ino
@@ -1,38 +1,39 @@
 #include "DigiKeyboard.h"
 
+int LED_PIN = 1;
+int KEY_DELAY = 1000;
+
+
 void setup() {
-  // don't need to set anything up to use DigiKeyboard
+  pinMode(LED_PIN, OUTPUT);
 }
 
+
 void loop() {
-  // this is generally not necessary but with some older systems it seems to
-  // prevent missing the first character after a delay:
+  digitalWrite(LED_PIN, HIGH);
+  
   DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(KEY_DELAY);
   
-  // Type out this string letter by letter on the computer (assumes US-style
-  // keyboard)
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
-  DigiKeyboard.delay(200);
-  DigiKeyboard.print("/Applications/Microsoft Outlook.app");
-  DigiKeyboard.delay(2000);
+  // Minimizar todo
+  DigiKeyboard.sendKeyStroke(KEY_W, MOD_GUI_LEFT);
+  DigiKeyboard.delay(KEY_DELAY);
+
+  // Ejecutar CMD
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(KEY_DELAY);
+  DigiKeyboard.print("cmd");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2500);
-  DigiKeyboard.sendKeyStroke(KEY_N, MOD_GUI_LEFT | MOD_ALT_LEFT);
-  DigiKeyboard.delay(200);
-  DigiKeyboard.println("your@email.com");
-  DigiKeyboard.delay(200);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.delay(200);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.print("Lunch is on me!");
-  DigiKeyboard.delay(200);
-  DigiKeyboard.print("\t");
-  DigiKeyboard.print("I'm feeling generous so I'll be buying everyone lunch for a week.");
-  DigiKeyboard.delay(200);
-  DigiKeyboard.sendKeyStroke(MOD_GUI_LEFT, KEY_ENTER);
+  DigiKeyboard.delay(KEY_DELAY);
+
+  // Abrir notepad y hola mundo
+  DigiKeyboard.print("notepad");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(KEY_DELAY);
   
-  // It's better to use DigiKeyboard.delay() over the regular Arduino delay()
-  // if doing keyboard stuff because it keeps talking to the computer to make
-  // sure the computer knows the keyboard is alive and connected
-  DigiKeyboard.delay(10000);
+  DigiKeyboard.println("Hola mundo");
+
+  digitalWrite(LED_PIN, LOW);
+
+  for(;;){ /*empty*/ }
 }
